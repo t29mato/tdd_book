80562589ea28c5204591f9ff65c77bc1e56f36fa
mato <tomoya.matou@gmail.com>
2018-11-13 00:13:17 +0900

    #8

--- src/Money.php
+++ src/Money.php
@@ -2,13 +2,24 @@
 
 namespace Money;
 
-class Money
+abstract class Money
 {
     protected $amount;
+    abstract function times(int $multiplier);
 
     public function equals(Money $money)
     {
         return $this->amount === $money->amount
             && get_class($this) == get_class($money);
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
     }
 }
